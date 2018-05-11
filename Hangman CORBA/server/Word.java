40497f46d7c7cecfc5bfb3b62f35eef35e7ca114
Jonelle <34231235+2163797@users.noreply.github.com>
2018-05-12 07:35:03 +0800

    Added the Word.java file - Carino

---
+++ Hangman CORBA/server/Word.java
@@ -0,0 +1,13 @@
+package classes;
+import java.io.Serializable;
+
+public class Word implements  Serializable {
+	private String word;
+	public Word(String word) {
+		this.word = word;
+	}
+
+	public String getWord(){
+		return word;
+	}
+}
